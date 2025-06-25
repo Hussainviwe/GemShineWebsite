0ad56f94ca41ddcbd7b54353083bb9b4b2d172bb
Hussainviwe <mhusmanhussain@gmail.com>
2025-06-26 00:06:10 +0530

    check the api working

--- api/index.js
+++ api/index.js
@@ -14,3 +14,9 @@ const app = express();
 app.listen(3000, () => {
   console.log("Server is running on http://localhost:3000");
 });
+
+// app.get("/test", (req, res) => {
+//   res.json({
+//     message: " API is working",
+//   });
+// });
